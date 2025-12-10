fa94f63171f42ec4a9e91f6a815635a5b8ffd0c7
Shoaib Mohkh <shoaibmohkh@gmail.com>
2025-12-10 17:46:38 +0300

    edit on Config header and add examples

--- include/Config.hpp
+++ include/Config.hpp
@@ -36,6 +36,8 @@ class LocationConfig {
         std::string                        uploadStore;       // Upload storage path (/var/www/uploads)
         std::vector<std::string>           allowMethods;      // Allowed methods (GET, POST, DELETE)
         std::map<std::string, std::string> cgiExtensions;     // CGI (.py, /usr/bin/python3)
+
+        LocationConfig() : returnCode(0), uploadEnable(false), autoindex(false) {}
 };
 
 // server {
@@ -53,6 +55,62 @@ class ServerConfig {
         std::string                              root;                 // Document root (./www)
         std::string                              index;                // Index file (index.html)
         std::map<int, std::string>               error_Pages;          // Error pages (404, /errors/404.html)
+        std::vector<LocationConfig>              locations;            // Location configurations
+
+        ServerConfig() : port(80), client_Max_Body_Size(0), root("./www"), index("index.html") {}
 };
+
+// server {
+//     listen 8080;
+//     server_name mysite;
+//     root ./site1;
+// }
+// server {
+//     listen 9090;
+//     server_name api;
+//     root ./apiroot;
+// }
+// GlobalConfig
+//    ├── ServerConfig #1
+//    │       ├── LocationConfig /images
+//    │       ├── LocationConfig /upload
+//    │       └── ...
+//    └── ServerConfig #2
+//            ├── LocationConfig /api
+//            ├── LocationConfig /static
+//            └── ...
+class Config {
+    public:
+        std::vector<ServerConfig> servers; // List of server configurations
+};
+
+// full example for config file
+// server {
+//     listen 8080;                      ==>     ServerConfig::port
+//     root ./www;                       ==>     ServerConfig::root
+//     index index.html;                 ==>     ServerConfig::index
+//     client_max_body_size 1000000;     ==>     ServerConfig::client_Max_Body_Size
+//     error_page 404 /errors/404.html;  ==>     ServerConfig::error_Pages[404] = returnPath::/errors/404.html
+
+//     location /images {
+//         autoindex on;                       ==>     LocationConfig::autoindex
+//         allow_methods GET POST;             ==>    LocationConfig::allowMethods ["GET", "POST"]
+//         upload_enable on;                   ==>    LocationConfig::uploadEnable
+//         upload_store /var/www/uploads;      ==>    LocationConfig::uploadStore
+//         cgi_extension .py /usr/bin/python3; ==>    LocationConfig::cgiExtensions[".py"] = "/usr/bin/python3";
+//         return 301 /new_images;             ==>    LocationConfig::returnCode = 301; LocationConfig::returnPath = "/new_images";
+//     }
+
+//     location /upload {
+//         autoindex off;                 ==>     LocationConfig::autoindex
+//         allow_methods POST;            ==>    LocationConfig::allowMethods ["POST"]
+//         upload_enable on;              ==>    LocationConfig::uploadEnable
+//         upload_store /var/www/uploads; ==>    LocationConfig::uploadStore
+//     }
+// }
+//  *** server.locations = {
+//          LocationConfig (path="/images", autoindex=true, allowMethods={"GET","POST"}, uploadEnable=true, uploadStore="/var/www/uploads", cgiExtensions={".py":"/usr/bin/python3"}, returnCode=301, returnPath="/new_images"), // LocationConfig for /images
+//          LocationConfig (path="/upload", autoindex=false, allowMethods={"POST"}, uploadEnable=true, uploadStore="/var/www/uploads")  // LocationConfig for /upload
+//     };
 
 #endif
