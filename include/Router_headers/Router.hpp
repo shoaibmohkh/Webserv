0952e78c3ec85633f8ef397ee46bf3ce9a9773b7
Shoaib Mohkh <shoaibmohkh@gmail.com>
2025-12-11 06:09:50 +0300

    create router header file

---
+++ include/Router_headers/Router.hpp
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Router.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/11 02:25:33 by marvin            #+#    #+#             */
+/*   Updated: 2025/12/11 02:25:33 by marvin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ROUTER_HPP
+#define ROUTER_HPP
+
+#include "HttpTypes.hpp"
+#include "../config_headers/Config.hpp"
+#include <iostream>
+#include <algorithm>
+#include <sstream>
+#include <dirent.h>
+#include <sys/stat.h>
+
+class Router
+{
+    public:
+        Router(const Config& config);
+        ~Router();
+        HTTPResponse handle_route_Request(const HTTPRequest& request) const;
+    private:
+        const Config& _config;
+        const ServerConfig &find_server_config(const HTTPRequest& request) const;
+        const LocationConfig* find_location_config(const std::string &uri, const ServerConfig& server_config) const;
+        bool is_method_allowed(const LocationConfig& location_config, HTTPMethod method) const;
+        std::string method_to_string(HTTPMethod method) const;
+        std::string to_string(int value) const;
+        std::string final_path(const ServerConfig& server_config, const LocationConfig& location_config, const std::string& uri) const;
+        HTTPResponse generate_autoindex_response(const std::string& path) const;
+};
+
+
+#endif
