fd2e0bb85b92750a7005e54db3d7a22869edd597
Shoaib Mohkh <shoaibmohkh@gmail.com>
2025-12-11 20:30:38 +0300

    edit on parser header file

--- include/config_headers/parser.hpp
+++ include/config_headers/parser.hpp
@@ -24,11 +24,14 @@ class Parser {
         LocationConfig         location_parse(int &_pos);
         ServerConfig           server_parse(int &_pos);
         void                   error_msg(int num);
+        void                   skip_directive(int &_pos);
         int                    uploadEnable_and_autoindex_parse(int &_pos, LocationConfig &locConfig);
         int                    port_and_clientMaxBodySize_parse(int &_pos, ServerConfig &serverConfig);
         int                    return_parse(int &_pos, LocationConfig &locConfig);
         int                    upload_parse(int &_pos, LocationConfig &locConfig);
-        int                    root_index_parse(int &_pos, ServerConfig &serverConfig);   
+        int                    root_index_parse(int &_pos, ServerConfig &serverConfig);
+        int                    location_root_parse(int &_pos, LocationConfig &locConfig);
+        int                    server_name_parse(int &_pos, ServerConfig &serverConfig);
         int                    cgi_extension_parse(int &_pos, LocationConfig &locConfig);
         int                    allow_methods_parse(int &_pos, LocationConfig &locConfig);  
         int                    error_page_parse(int &_pos, ServerConfig &serverConfig);
