b25bd4377060af38d78822b4fb543fcdc58b4e6a
Shoaib Al-Kawaldeh <shoaibmohkh9@gmail.com>
2025-12-13 21:46:47 +0300

    edit on header name calling

--- src/config/location_parser.cpp
+++ src/config/location_parser.cpp
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../../include/config_headers/parser.hpp"
+#include "../../include/config_headers/Parser.hpp"
 
 int Parser::uploadEnable_and_autoindex_parse(int &_pos, LocationConfig &locConfig)
 {
